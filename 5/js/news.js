2069f6655824186e2c6c8f55b2742b1795a08b97
lsyaaaaaaa <lsyaaaaaaa@163.com>
2020-07-13 02:18:08 +0800

    作业

---
+++ 5/js/news.js
@@ -0,0 +1,37 @@
+  
+$(document).ready(function () {
+
+    function get(page) {
+        $('#loading').css('display', 'block');
+        axios.get('http://mock-api.com/rnNWOLKl.mock/news?page=' + page)
+            .then(function (res) {
+                console.log("res", res.data);
+                var i;
+                var item;
+                $('#introduce tr:not(:first-child)').remove();
+                for (i = 0; i < res.data.length; ++i) {
+                    item = res.data[i];
+                    console.log('item', item);
+                    $('#introduce').append('<tr  style="color: rgb(39, 139, 197);">' +
+                        '<td> <img " src="' + item.imgurl + ' " </td>' +
+                        '<td style="padding:30px;">' + item.name + '</td>' +
+                        '<td style="padding:30px;">' + item.sonority + '</td>' +
+                        '<td style="padding:30px;"> <a href="' + item.url + '"><button class="btn btn-primary bg-warning">大致内容</button></a> </td>' +
+                        '</tr>');
+                }
+                $('#loading').css('display', 'none');
+
+            })
+            .catch(function (e) {
+                console.log('e', e);
+            });
+    }
+    get(1);
+    $('#pagination button').click(function (){
+        var page = Number($(this).text());
+        get(page);
+        $('#pagination button').removeClass('active');
+        $(this).addClass('active');
+    });
+
+});
